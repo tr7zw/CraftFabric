271361c10abbaa0018ad7a33883c1dbaf353618c
Gabriele C <sgdc3.mail@gmail.com>
2019-03-27 23:54:40 +0100

    Universal

--- src/main/java/io/github/tr7zw/fabricbukkit/mixin/ExampleMixin.java
+++ src/main/java/io/github/tr7zw/fabricbukkit/mixin/ExampleMixin.java
@@ -8,6 +8,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(MinecraftClient.class)
 public class ExampleMixin {
+
     @Inject(at = @At("HEAD"), method = "init()V")
     private void init(CallbackInfo info) {
         System.out.println("This line is printed by an example mod mixin!");
