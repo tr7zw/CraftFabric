a2e6c563da31ef38c99020d914bbf40f7161f42b
Gabriele C <sgdc3.mail@gmail.com>
2019-03-27 23:49:45 +0100

    Restore examples

--- src/main/java/io/github/tr7zw/fabricbukkit/FabricBukkit.java
+++ src/main/java/io/github/tr7zw/fabricbukkit/FabricBukkit.java
@@ -1,10 +1,7 @@
 package io.github.tr7zw.fabricbukkit;
 
-import org.bukkit.Server;
-
 import com.mojang.brigadier.StringReader;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
-
 import net.fabricmc.api.ModInitializer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
@@ -12,29 +9,28 @@ import net.minecraft.nbt.CompoundTag;
 import net.minecraft.sortme.JsonLikeTagParser;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class FabricBukkit implements ModInitializer {
 
     public static final Item FABRIC_ITEM = new Item(new Item.Settings().itemGroup(ItemGroup.MISC));
+    private static final Logger LOG = LogManager.getLogger();
 
     @Override
     public void onInitialize() {
-        System.out.println("Hello Fabric world!");
+        LOG.info("FabricBukkit is starting...");
         Registry.register(Registry.ITEM, new Identifier("my-mod", "fabric_item"), FABRIC_ITEM);
 
-        Server server = new ServerImpl();
-        System.out.println(server.getName());
+        LOG.info(Registry.BLOCK.get(1).getTranslationKey());
 
-        System.out.println(Registry.BLOCK.get(1).getTranslationKey());
-
-        System.out.println(org.bukkit.Material
-            .getMaterial(Registry.BLOCK.get(1).getTranslationKey().replace("block.minecraft.", "minecraft:")));
+        LOG.info(org.bukkit.Material.getMaterial(Registry.BLOCK.get(1).getTranslationKey().replace("block.minecraft.", "minecraft:")));
 
         try { // Found the nbt parser
             CompoundTag tag = (CompoundTag) (new JsonLikeTagParser(new StringReader("{" + "    \"additionalData\": {"
-                + "        \"Invisible\": 0,"
-                + "        \"Pose\": \"{Body:[41f,0f,0f],Head:[0f,52f,52f],LeftLeg:[105f,64f,29f],RightLeg:[37f,0f,0f]}\""
-                + "    }" + "}"))).parseTag();
+                    + "        \"Invisible\": 0,"
+                    + "        \"Pose\": \"{Body:[41f,0f,0f],Head:[0f,52f,52f],LeftLeg:[105f,64f,29f],RightLeg:[37f,0f,0f]}\""
+                    + "    }" + "}"))).parseTag();
             System.out.println(tag.getCompound("additionalData").getTag("Pose").getClass().getSimpleName());
         } catch (CommandSyntaxException e) {
             // TODO Auto-generated catch block
---
+++ src/main/java/io/github/tr7zw/fabricbukkit/mixin/MixinEntity.java
@@ -0,0 +1,24 @@
+package io.github.tr7zw.fabricbukkit.mixin;
+
+import net.minecraft.entity.Entity;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(Entity.class)
+public class MixinEntity {
+
+    // Example for fields
+
+    /*
+     * @Shadow protected Random rand;
+     *
+     * @Shadow public boolean isAirBorne;
+     *
+     * @Shadow public double motionX;
+     *
+     * @Shadow public double motionY;
+     *
+     * @Shadow public double motionZ;
+     *
+     * @Shadow public boolean onGround;
+     */
+}
