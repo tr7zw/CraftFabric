9322c45b3c146c507f2e8acfc3c69d6874df575b
Gabriele C <sgdc3.mail@gmail.com>
2019-03-30 23:15:44 +0100

    Start implementing Block

--- src/main/java/io/github/tr7zw/fabricbukkit/craftfabric/CraftMagicNumbers.java
+++ src/main/java/io/github/tr7zw/fabricbukkit/craftfabric/CraftMagicNumbers.java
@@ -1,5 +1,9 @@
 package io.github.tr7zw.fabricbukkit.craftfabric;
 
+import io.github.tr7zw.fabricbukkit.craftfabric.util.NamespaceUtils;
+import net.minecraft.block.Block;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.UnsafeValues;
@@ -9,13 +13,48 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.material.MaterialData;
 import org.bukkit.plugin.InvalidPluginException;
 import org.bukkit.plugin.PluginDescriptionFile;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
 
 @SuppressWarnings("deprecation")
 public final class CraftMagicNumbers implements UnsafeValues {
     public static final UnsafeValues INSTANCE = new CraftMagicNumbers();
 
-    private CraftMagicNumbers() {}
+    // Mappings
+    private static final Map<Block, Material> BLOCK_MATERIAL = new HashMap<>();
+    private static final Map<Material, Block> MATERIAL_BLOCK = new HashMap<>();
 
+    private CraftMagicNumbers() {
+    }
+
+    static {
+        for (Block block : Registry.BLOCK) {
+            BLOCK_MATERIAL.put(block, Material.getMaterial(Registry.BLOCK.getId(block).getPath().toUpperCase(Locale.ROOT)));
+        }
+        for (Material material : Material.values()) {
+            Identifier key = key(material);
+            MATERIAL_BLOCK.put(material, Registry.BLOCK.get(key));
+        }
+    }
+
+    public static @NotNull Material getMaterial(Block block) {
+        return BLOCK_MATERIAL.get(block);
+    }
+
+    public static @NotNull Block getBlock(Material material) {
+        return MATERIAL_BLOCK.get(material);
+    }
+
+    public static @NotNull Identifier key(Material mat) {
+        if (mat.isLegacy()) {
+            throw new IllegalArgumentException("Legacy materials aren't supported!");
+        }
+
+        return NamespaceUtils.toMinecraft(mat.getKey());
+    }
 
     /**
      * This helper class represents the different NBT Tags.
@@ -42,75 +81,75 @@ public final class CraftMagicNumbers implements UnsafeValues {
 
     @Override
     public Material toLegacy(Material material) {
-	// TODO Auto-generated method stub
-	return null;
+        // TODO Auto-generated method stub
+        return null;
     }
 
 
     @Override
     public Material fromLegacy(Material material) {
-	// TODO Auto-generated method stub
-	return null;
+        // TODO Auto-generated method stub
+        return null;
     }
 
 
     @Override
     public Material fromLegacy(MaterialData material) {
-	// TODO Auto-generated method stub
-	return null;
+        // TODO Auto-generated method stub
+        return null;
     }
 
 
     @Override
     public Material fromLegacy(MaterialData material, boolean itemPriority) {
-	// TODO Auto-generated method stub
-	return null;
+        // TODO Auto-generated method stub
+        return null;
     }
 
 
     @Override
     public BlockData fromLegacy(Material material, byte data) {
-	// TODO Auto-generated method stub
-	return null;
+        // TODO Auto-generated method stub
+        return null;
     }
 
 
     @Override
     public int getDataVersion() {
-	// TODO Auto-generated method stub
-	return 0;
+        // TODO Auto-generated method stub
+        return 0;
     }
 
 
     @Override
     public ItemStack modifyItemStack(ItemStack stack, String arguments) {
-	// TODO Auto-generated method stub
-	return null;
+        // TODO Auto-generated method stub
+        return null;
     }
 
 
     @Override
     public void checkSupported(PluginDescriptionFile pdf) throws InvalidPluginException {
-	// Nah
+        // Nah
     }
 
 
     @Override
     public byte[] processClass(PluginDescriptionFile pdf, String path, byte[] clazz) {
-	return clazz; //no processing
+        return clazz; //no processing
     }
 
 
     @Override
     public Advancement loadAdvancement(NamespacedKey key, String advancement) {
-	// TODO Auto-generated method stub
-	return null;
+        // TODO Auto-generated method stub
+        return null;
     }
 
 
     @Override
     public boolean removeAdvancement(NamespacedKey key) {
-	// TODO Auto-generated method stub
-	return false;
+        // TODO Auto-generated method stub
+        return false;
     }
 }
