64c960e9c4141680c8c497bc57fb66ad7a0699bb
tr7zw <tr7zw@live.de>
2019-11-03 01:47:32 +0100

    Change serverbrand to "FabricBukkit"

--- src/main/java/io/github/craftfabric/craftfabric/mixin/impl/MinecraftServerMixin.java
+++ src/main/java/io/github/craftfabric/craftfabric/mixin/impl/MinecraftServerMixin.java
@@ -6,6 +6,7 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import io.github.craftfabric.craftfabric.scheduler.CraftScheduler;
 import net.minecraft.server.MinecraftServer;
@@ -21,4 +22,10 @@ public class MinecraftServerMixin {
     	((CraftScheduler)Bukkit.getServer().getScheduler()).mainThreadHeartbeat(ticks + 1);
     }
 
+    @Inject(at = @At("HEAD"), method = "getServerModName", cancellable=true)
+    public String getServerModName(CallbackInfoReturnable<String> ret) {
+    	ret.setReturnValue("FabricBukkit");
+    	return "FabricBukkit";
+    }
+    
 }
